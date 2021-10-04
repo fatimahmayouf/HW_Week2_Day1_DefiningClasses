3500accfc471360e72e9a18aad67968cc9c92576
fatimahmayouf <ftmmtf1995@gmail.com>
2021-10-04 08:44:09 +0300

    fixed issue

--- HW2_1/src/main/kotlin/Soltion#1.kt
+++ HW2_1/src/main/kotlin/Soltion#1.kt
@@ -3,14 +3,13 @@ fun main() {
 
     car_1.apply {
         model = "toyota  "
-        year = 2020
     }
     println(car_1.model + car_1.year)
 }
 
-class Car(year: Int) {
+class Car(var year: Int) {
     lateinit var model: String
 
-    var year: Int? = null
+
 
 }
--- HW2_1/src/main/kotlin/solution#2.kt
+++ HW2_1/src/main/kotlin/solution#2.kt
@@ -1,7 +1,8 @@
 fun main() {
     var book1 = Books("Game of Thrones", "bbGeorge Martin", 54.0)
-
-    println("${book1}")
+    println("Name:${book1.title}")
+    println("auther:${book1.auther}")
+    println("price:\$${book1.price}")
 
 
          println(book1.bookDetails(true))
@@ -9,11 +10,14 @@ fun main() {
 
 
     var book2 = Books("We Were Liars", "E. Lockhart", 30.00)
-    println("$book2")
+    println("Name:${book2.title}")
+    println("auther:${book2.auther}")
+    println("price:\$${book2.price}")
+
     println(book2.bookDetails(false))
 }
 
-class Books(title: String, auther: String, price: Double) {
+class Books(var title: String,var auther: String,var  price: Double) {
 
     fun bookDetails(inStock: Boolean): String {
         if (inStock == true)
